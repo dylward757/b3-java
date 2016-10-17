69fccf197a3bf1f2ede54143897d8489c0e09c6c
dylward757 <dylward757@gmail.com>
2016-10-17 12:52:36 -0600

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,67 +1,45 @@
-import javax.swing.JOptionPane;
-
-/*
- * Dylan Ward
- * 8/25/16
- * 3B
- * Introduction to JOPs - JOptionPanes
- * 
- */
 
 public class Runner {
 
 	public static void main(String[] args) {
 		
-		//place your field variables here, at the top of the method
+		String panel_title = "Yo login pretty please";
 		
-		String first_name = "";
-		String last_name = "";
-		String showMsg = "";
-	    String age_string = "";
-	    int age = 0;
+		TextFieldExample login01 = new TextFieldExample(panel_title);
+		do{					
+			login01.createAndShowGUI();
+		} while(login01.isLoginCorrect() !=  true);
 		
-	    // convert strings to floats
-	    String length_string = "";
-	    float length = 0;
-	    
-	    String width_string = "";
-	    float width = 0;
-	    
-	    String height_string = "";
-	    float height = 0;
-	    
-	    
-		first_name = JOptionPane.showInputDialog("What is your first name?");
 		
-		last_name = JOptionPane.showInputDialog("What is your last name?");
 		
-		//System.out.println(first_name);
+		// check for a successful login
 		
-		showMsg = "Nice to meet you " + first_name + " " + last_name;
+		boolean goodLogin = login01.isLoginCorrect();
+		System.out.println("Do we have a good login: " + goodLogin);
 		
-		JOptionPane.showMessageDialog(null, showMsg);
 		
-		age_string = JOptionPane.showInputDialog("How old are you?");
-		//System.out.println("age_string: " + age_string);
 		
-		age = Integer.parseInt(age_string);		
-		age = age + 5;
-		//System.out.println("int age: " + age);
+		// if good login, then launch another program
 		
-		length_string = JOptionPane.showInputDialog("What is the length of that box?");
-		width_string = JOptionPane.showInputDialog("What is the width of that box?");
-		height_string = JOptionPane.showInputDialog("What is the height of that box?");
-		length = Float.parseFloat(length_string);
-		width = Float.parseFloat(width_string);
-		height = Float.parseFloat(height_string);
-		//System.out.println("length: " + length);
-		//System.out.println("width: " + width);
-		//System.out.println("height: " + height);
+		System.out.println("Good login! now launch another class");
 		
-		JOptionPane.showInputDialog(null, "The volume of the box is = " + length*width*height);
 		
-		//volume = length*width*height
-		System.exit(0);
+		/*
+		 * Yes, you can rewrite class variables
+		 * but you are asking for reliability and bug problems,
+		 * instead always use setters and getters
+		 
+		
+		int test = login01.intPractice;
+		System.out.println("login01.intPractice: " + test);
+		
+		login01.intPractice = 20;
+		System.out.println("login01.intPractice: " + login01.intPractice);
+		
+		
+		login01.getIntPractice();
+		login01.setIntPractice(20);
+		*/
 	}
 
 }
